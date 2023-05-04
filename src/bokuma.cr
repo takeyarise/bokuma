bfcda7a13b4e4029c0e854de0eb025153ac4de7e
torimoti <take1010ct@gmail.com>
2023-05-04 21:25:03 +0900

    Update bokuma.cr

--- src/bokuma.cr
+++ src/bokuma.cr
@@ -1,4 +1,7 @@
 # TODO: Write documentation for `Bokuma`
+# 参考になるかも:
+# https://github.com/Angarsk8/realtime-todo-app
+# https://crystal-jp.github.io/introducing-crystal/chapters/06-web-development.html
 module Bokuma
   VERSION = "0.1.0"
 
